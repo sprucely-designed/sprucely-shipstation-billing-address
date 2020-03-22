c7c3fea5442d3e333a55cdde9ba5968491b9ebfc
dveth <kanosauries@gmail.com>
2020-03-22 15:08:59 -0500

    Updated fixes

--- sprucely-shipstation-billing-address.php
+++ sprucely-shipstation-billing-address.php
@@ -1,3 +1,4 @@
+@@ -0,0 +1,73 @@
 <?php
 
 /**
@@ -13,6 +14,19 @@
 if ( defined( 'ABSPATH' ) ) {
 	die;
 }
+
+$billing_meta_key = 'SSBillingInfo';
+
+	add_filter( 'woocommerce_shipstation_export_custom_field_2', 'shipstation_custom_field_2' );
+	/**
+	 * Update custom field 2 that is sent to SS.
+	 *
+	 * @see https://gist.github.com/woogists/53d13508835276e66578f9c6f2398d03#file-wc-export-custom-field-data-php
+	 * @return string Meta Key
+	 */
+	function shipstation_custom_field_2() {
+		return $billing_meta_key; // Replace this with the key of your custom field.
+	}
 
 add_action( 'woocommerce_checkout_create_order', 'sprucely_add_billing_address' );
 
@@ -40,19 +54,6 @@ function sprucely_add_billing_address( $order, $sent_to_admin, $plain_text, $ema
 		$billingaddress = $billingaddress1 . '\n' . $billingaddress2 . '\n' . $billingcity . ', ' . $billingstate . ' ' . $billingcountry;
 		$billingname = $order->get_billing_first_name() . ' ' . $order->get_billing_last_name();
 	*/
-
-	$billing_meta_key = 'SSBillingInfo';
-
-	add_filter( 'woocommerce_shipstation_export_custom_field_2', 'shipstation_custom_field_2' );
-	/**
-	 * Update custom field 2 that is sent to SS.
-	 *
-	 * @see https://gist.github.com/woogists/53d13508835276e66578f9c6f2398d03#file-wc-export-custom-field-data-php
-	 * @return string Meta Key
-	 */
-	function shipstation_custom_field_2() {
-		return $billing_meta_key; // Replace this with the key of your custom field.
-	}
 
 	if ( $billing_address != $shipping_address ) {
 
